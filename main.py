a95145bc61c76bd8c83b4e5c3977adfaf0e045ac
Fuystik <160339015+Fuystik@users.noreply.github.com>
2024-09-04 21:16:39 +0500

    Update main.py
    добавил возможность сохранения фото отправленное пользователем

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ def handle_start(message):
 
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
-    # Обработка фотографии
+    # сохранение фотографии
+    photo = message.photo[-1]
+    file_info = bot.get_file(photo.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    save_path = 'photo.jpg'
+    with open(save_path, 'wb') as new_file:
+        new_file.write(downloaded_file)
     bot.send_message(message.chat.id, "Спасибо за изображение!")
 
 
